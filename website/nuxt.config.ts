7fbb88540ef5ded1c1603f7dd62be90fe241ac4a
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-23 00:35:20 +0300

    build(website): add module `@nuxt/i18n`

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+
     hooks: {
         'prerender:routes'({ routes }) {
             routes.clear()
         },
     },
+
+    modules: ['@nuxtjs/i18n'],
 })
