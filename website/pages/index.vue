6540aee067779c47beaf30e39c6d620666b7372f
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-23 11:06:13 +0300

    feat(pages): add color pallete table to the home page

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -5,5 +5,9 @@ definePageMeta({
 </script>
 
 <template>
-    <main></main>
+    <main>
+        <h1>{{ $t('home.heading') }}</h1>
+        <p>{{ $t('home.tagline') }}</p>
+        <ColorPalette />
+    </main>
 </template>
