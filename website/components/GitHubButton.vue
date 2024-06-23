9745097618feb55f82b5ae63d9b5136b95f40db9
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-23 03:53:24 +0300

    feat(components): add `GitHubButton`

---
+++ website/components/GitHubButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <NuxtLink
+        class="button"
+        to="https://github.com/pinkphantasm/theme"
+        external
+        target="_blank"
+    >
+        <Icon class="icon" name="carbon:logo-github" size="28" />
+    </NuxtLink>
+</template>
+
+<style scoped>
+.button {
+    transition: var(--transition-color);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 36px;
+    height: 36px;
+    background-color: transparent;
+    border-radius: 4px;
+}
+
+.button:hover {
+    background-color: var(--selection);
+}
+
+.icon,
+.icon * {
+    color: var(--foreground);
+}
+</style>
