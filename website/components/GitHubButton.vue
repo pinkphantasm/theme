77606ccf024464107326954166770e08fcbe7a7d
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-23 07:35:05 +0300

    refactor(components): separate `UiButtonLink`

--- website/components/GitHubButton.vue
+++ website/components/GitHubButton.vue
@@ -1,27 +1,10 @@
 <template>
-    <NuxtLink
+    <UiButtonLink
         class="button"
         to="https://github.com/pinkphantasm/theme"
         external
         target="_blank"
     >
         <Icon name="carbon:logo-github" size="28" />
-    </NuxtLink>
+    </UiButtonLink>
 </template>
-
-<style scoped>
-.button {
-    transition: var(--transition-color);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 36px;
-    height: 36px;
-    background-color: transparent;
-    border-radius: 4px;
-}
-
-.button:hover {
-    background-color: var(--selection);
-}
-</style>
---
+++ website/components/ui/ButtonLink.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface Props {
+    to: string
+    target: '_blank' | '_self' | '_top' | '_parent'
+    external: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+    target: '_self',
+    external: false,
+})
+</script>
+
+<template>
+    <NuxtLink class="button" :to="to" :external="external" :target="target">
+        <slot />
+    </NuxtLink>
+</template>
+
+<style scoped>
+.button {
+    transition: var(--transition-color);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 36px;
+    height: 36px;
+    background-color: transparent;
+    border-radius: 4px;
+}
+
+.button:hover {
+    background-color: var(--selection);
+}
+</style>
