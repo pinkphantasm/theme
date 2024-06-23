bab9f4f34a4497e818692cbba448d58ebfa3b2ff
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-23 03:51:41 +0300

    feat(components): add `ChangeColorModeButton`

---
+++ website/components/ChangeColorModeButton.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const switchColorMode = () => {
+    if (colorMode.preference === 'system') {
+        colorMode.preference = 'light'
+        return
+    }
+
+    if (colorMode.preference === 'light') {
+        colorMode.preference = 'dark'
+        return
+    }
+
+    colorMode.preference = 'system'
+}
+</script>
+
+<template>
+    <ClientOnly>
+        <button class="button" @click="switchColorMode">
+            <Icon
+                class="icon"
+                name="carbon:screen"
+                size="28"
+                v-if="$colorMode.preference === 'system'"
+            />
+            <Icon
+                class="icon"
+                name="carbon:sun"
+                size="28"
+                v-else-if="$colorMode.preference === 'light'"
+            />
+            <Icon class="icon" size="28" v-else name="carbon:moon" />
+        </button>
+        <template #fallback>
+            <button class="button">
+                <Icon class="icon" name="carbon:circle-dash" size="28" />
+            </button>
+        </template>
+    </ClientOnly>
+</template>
+
+<style scoped>
+.button {
+    transition: var(--transition-color);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 36px;
+    height: 36px;
+    background-color: transparent;
+    border-radius: 4px;
+}
+
+.button:hover {
+    background-color: var(--selection);
+}
+
+.icon,
+.icon * {
+    color: var(--foreground);
+}
+</style>
